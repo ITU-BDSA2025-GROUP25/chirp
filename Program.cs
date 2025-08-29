c070744ee2eba4e0352b651201ec1fa105ced145
Anton Krøis <74717805+pallestilk@users.noreply.github.com>
2025-08-29 09:46:56 +0200

    Files added

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿List<string> cheeps = new() { "Hello, BDSA students!", "Welcome to the course!", "I hope you had a good summer." };
+
+foreach (var cheep in cheeps)
+{
+    Console.WriteLine(cheep);
+    Thread.Sleep(1000);
+}
