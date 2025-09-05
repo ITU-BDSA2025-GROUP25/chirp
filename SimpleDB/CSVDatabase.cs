49a0a9a9afdc9a8dbf79a059c59c3ff32c9e2449
Anton <antk@itu.dk>
2025-09-05 11:57:22 +0200

    CSVDatabase implements interface

--- SimpleDB/CSVDatabase.cs
+++ SimpleDB/CSVDatabase.cs
@@ -1,6 +1,15 @@
 ﻿namespace Chirp.CLI.SimpleDB;
 
-public class CSVDatabase<T> 
+public class CSVDatabase<T> : IDatabaseRepository<T>
 {
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Store(T record)
+    {
+        throw new NotImplementedException();
+    }
     
 }
