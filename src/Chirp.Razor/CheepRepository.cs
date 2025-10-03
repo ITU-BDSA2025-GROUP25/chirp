ed169586673357bfcf5312e3757acb53a2325807
Anton <antk@itu.dk>
2025-10-03 13:05:51 +0200

    CheepRepository setup

---
+++ src/Chirp.Razor/CheepRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace Chirp.Razor;
+
+public interface ICheepRepository
+{
+    Task CreateCheep(Cheep newCheep);
+    Task<List<Cheep>> ReadCheep(String authorName);
+    Task UpdateCheep(Cheep alteredCheep);
+}
+
+public class CheepRepository : ICheepRepository
+{
+    public Task CreateCheep(Cheep newCheep)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Cheep>> ReadCheep(String authorName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateCheep(Cheep alteredCheep)
+    {
+        throw new NotImplementedException();
+    }
+}
