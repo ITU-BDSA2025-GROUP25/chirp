2e3dface855f8811319452e0aba2a96ae8e0d701
Anton <antk@itu.dk>
2025-10-03 11:18:59 +0200

    Chirp.Razor data model

---
+++ src/Chirp.Razor/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace Chirp.Razor;
+
+public class Author
+{
+    public string name { get; set; }
+    
+    public string email { get; set; }
+    
+    public ICollection<Cheep> cheeps { get; set; }
+}
---
+++ src/Chirp.Razor/Cheep.cs
@@ -0,0 +1,10 @@
+﻿namespace Chirp.Razor;
+
+public class Cheep
+{
+    public string text { get; set; }
+    
+    public DateTime timeStamp { get; set; }
+    
+    public Author author { get; set; }
+}
