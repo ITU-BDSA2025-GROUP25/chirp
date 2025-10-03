d78606fcad8b9bf5f0bed7e0f999ee7c31946685
Anton <antk@itu.dk>
2025-10-03 12:08:50 +0200

    Create DataModel.cs

---
+++ src/Chirp.Razor/DataModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Chirp.Razor;
+
+public class Author
+{
+    public string name { get; set; }
+    
+    public string email { get; set; }
+    
+    public ICollection<Cheep> cheeps { get; set; }
+}
+
+public class Cheep
+{
+    public string text { get; set; }
+
+    public DateTime timeStamp { get; set; }
+
+    public Author author { get; set; }
+}
