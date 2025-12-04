66d863196ae106e4ac9eb2eef003817fbcd1dc85
Hedin Mortensen <hedinm01@hotmail.com>
2025-12-04 14:58:52 +0100

    Extend CheepService to support liking cheeps and populate like data
    Added LikeCheep and UnlikeCheep methods
    Inject ILikeRepository
    Populated like count and user state per CheepDTO

--- src/Chirp.Infrastructure/CheepService.cs
+++ src/Chirp.Infrastructure/CheepService.cs
@@ -9,13 +9,13 @@ public interface ICheepService
 
     Task LikeCheep(int cheepId, string username);
     Task UnlikeCheep(int cheepId, string username);
-
 }
 
 public class CheepService : ICheepService
 {
     private readonly ICheepRepository _cheepRepository;
     private readonly IFollowRepository _followRepository;
+    private readonly ILikeRepository _likeRepository;
 
     private const int PageSize = 32;
 
@@ -29,27 +29,43 @@ public class CheepService : ICheepService
         _likeRepository = likeRepository;
     }
 
+    //Public cheeps
     public async Task<List<CheepDTO>> GetCheeps(int page = 1)
     {
-        return await _cheepRepository.ReadCheep(page, PageSize);
+        var cheeps = await _cheepRepository.ReadCheep(page, PageSize);
+
+        await AddLikesToCheep(cheeps);
+
+        return cheeps;
     }
 
+    //Cheeps by author
     public async Task<List<CheepDTO>> GetCheepsFromAuthor(string author, int page = 1)
     {
-        return await _cheepRepository.ReadCheepByAuthor(author, page, PageSize);
-    }
+        var cheeps = await _cheepRepository.ReadCheepByAuthor(author, page, PageSize);
 
+        await AddLikesToCheep(cheeps);
+
+        return cheeps;
+    }
+    
+    //cheeps by privateTimeLine
     public async Task<List<CheepDTO>> GetPrivateTimeline(string username, int page = 1)
     {
         var following = await _followRepository.GetFollowing(username);
-        return await _cheepRepository.GetTimelineByAuthors(following, page);
-    }
+        var cheeps = await _cheepRepository.GetTimelineByAuthors(following, page);
 
+        await AddLikesToCheep(cheeps, username);
+
+        return cheeps;
+    }
+    
+    
     public async Task PostCheep(CheepDTO cheep)
     {
         await _cheepRepository.CreateCheep(cheep);
     }
-
+    
     public async Task LikeCheep(int cheepId, string username)
     {
         await _likeRepository.LikeCheep(cheepId, username);
@@ -60,4 +76,16 @@ public class CheepService : ICheepService
         await _likeRepository.UnlikeCheep(cheepId, username);
     }
 
+    
+
+    private async Task AddLikesToCheep(List<CheepDTO> cheeps, string? username = null)
+    {
+        foreach (var cheep in cheeps)
+        {
+            cheep.LikeCount = await _likeRepository.CountLikes(cheep.CheepId);
+
+            if (!string.IsNullOrEmpty(username))
+                cheep.HasLiked = await _likeRepository.HasLiked(cheep.CheepId, username);
+        }
+    }
 }
