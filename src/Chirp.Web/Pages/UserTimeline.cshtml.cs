63e76931d9ccf28403abdd197b5fdef4c2691525
Hedin Mortensen <hedinm01@hotmail.com>
2025-12-07 14:14:09 +0100

    Add pagination to user timeline and match pagination style

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -1,24 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Chirp.Razor.Pages;
-
-public class UserTimelineModel : PageModel
+namespace Chirp.Razor.Pages
 {
-    private readonly ICheepService _service;
-    public required List<CheepDTO> Cheeps { get; set; }
-
-    public UserTimelineModel(ICheepService service)
+    public class UserTimelineModel : PageModel
     {
-        _service = service;
+        private readonly ICheepService _service;
+        private const int PageSize = 32;
+
+        public required List<CheepDTO> Cheeps { get; set; }
+        public int CurrentPage { get; set; }
+        public bool HasMorePages { get; set; }
+
+        public UserTimelineModel(ICheepService service)
+        {
+            _service = service;
+        }
+
+        public async Task OnGet(string author, [FromQuery] int page = 1)
+        {
+            if (page < 1) page = 1;
+            CurrentPage = page;
+
+            Cheeps = await _service.GetCheepsFromAuthor(author, page);
+
+            // If we received a full page, assume more may exist
+            HasMorePages = Cheeps.Count == PageSize;
+        }
     }
-
- 	public async Task OnGet(string author, [FromQuery] int page = 1) //defauls to page 1 if page not given
-	{
-    if (page < 1) page = 1; // Ensuring no 0 or negative page numbers
-
-    Cheeps = await _service.GetCheepsFromAuthor(author, page);
-    
-
-	}
 }
