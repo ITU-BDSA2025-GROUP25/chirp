04e9b8c83cb25f3e7f0839284b7c5c2b07e8e5de
jblo <jblo@itu.dk>
2025-12-30 16:44:31 +0100

    testing azure

--- src/Chirp.Core/DataModel.cs
+++ src/Chirp.Core/DataModel.cs
@@ -76,7 +76,6 @@ public class AuthorDTO
     public required string Name { get; set; }
     public required string Email { get; set; }
 }
-
 // not currently used cause Author doesnt leave this layer
 public class FollowDTO
 {
